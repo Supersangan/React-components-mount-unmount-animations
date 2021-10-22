2e82f27513f71a5cc8d4584463f69378d519a383
Alexander Rekhviashvili <sangansuperman@ya.ru>
2021-10-22 16:12:03 +0300

    App cleaned, mount/unmount animations using react-spring added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './main.css';
+import {useState} from 'react';
+import {useTransition, animated} from 'react-spring';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const transition = useTransition(items, {
+    from: {x: -100, y: 500, opacity: 0, width: 10, height: 10},
+    enter: item => async (next) => {
+      await next({y: item.y, opacity: 1, delay: item.delay});
+      await next({x: 0, width: 100, height: 100});
+    },
+    leave: item => async (next) => {
+      await next({x: 100, width: 10, height: 10, delay: item.delay});
+      await next({y: 500, opacity: 0});
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={() => {
+        setItems(v => v.length ? [] : [
+          {y: -100, delay: 0},
+          {y: 0, delay: 200},
+          {y: 100, delay: 400},
+        ])
+      }}>
+        {items.length ? 'Unmount' : 'Mount'}
+      </button>
+
+      <div className="container">
+        {transition((style, item) =>
+          item ? <animated.div style={style} className="item" /> : ''
+        )}
+      </div>
     </div>
   );
 }
